f08cf7f97beb051ab4e3059d315ec739ae002cae
Tejal1386 <tejalmca1386@gmail.com>
2017-09-30 14:05:29 -0400

    delete student record using delete method

--- Student_Data.js
+++ Student_Data.js
@@ -27,14 +27,13 @@ server
 // Maps req.body to req.params so there is no switching between them
 .use(restify.bodyParser())
 
-
-// Create a new student record
-server.post('/sendPost', function (req, res, next) {
+//------------------------------------------------------------------------------//
+                       // Create a new student record
+//------------------------------------------------------------------------------//
+      server.post('/sendPost', function (req, res, next) {
     
         console.log("sendPost: sending response...");
-
-       
-        
+      
       // Make sure name is defined
       if (req.params.name === undefined ) {
         // If there are any errors, pass them to next in the correct format
@@ -60,8 +59,49 @@ server.post('/sendPost', function (req, res, next) {
       })
     })
 
-    // Update a Student Record by their id
-    server.put('/students/:id', function (req, res, next) {
+//------------------------------------------------------------------------------//
+                      // Get all student records in the system
+//------------------------------------------------------------------------------//
+
+        server.get('/sendGet', function (req, res, next) {
+  
+        console.log("sendGet: received request..");
+
+        // Find every entity within the given collection
+        studentsSave.find({}, function (error, students) {
+
+        // Return all of the users in the system
+        res.send(students)
+        })
+      })
+
+//------------------------------------------------------------------------------//
+                      // Get a single user by their student id
+//------------------------------------------------------------------------------//
+
+      server.get('/Students/:id', function (req, res, next) {
+    
+        // Find a single user by their id within save
+        studentsSave.findOne({ _id: req.params.id }, function (error, students) {
+    
+        // If there are any errors, pass them to next in the correct format
+        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+    
+        if (students) {
+          // Send the user if no issues
+          res.send(students)
+        } else {
+          // Send 404 header if the user doesn't exist
+          res.send(404)
+        }
+      })
+    })
+
+//------------------------------------------------------------------------------//
+                      // Update a Student Record by their id
+//------------------------------------------------------------------------------//
+
+      server.put('/students/:id', function (req, res, next) {
       
         // Make sure name is defined
         if (req.params.name === undefined ) {
@@ -79,8 +119,8 @@ server.post('/sendPost', function (req, res, next) {
               age: req.params.age
           }
         
-        // Update the user with the persistence engine
-        studentsSave.update(newStudent, function (error, student) {
+          // Update the user with the persistence engine
+          studentsSave.update(newStudent, function (error, student) {
       
           // If there are any errors, pass them to next in the correct format
           if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
@@ -90,3 +130,34 @@ server.post('/sendPost', function (req, res, next) {
         })
       })
       
+//------------------------------------------------------------------------------//
+                       // Delete student record with the given id
+//------------------------------------------------------------------------------//
+
+server.del('/students/:id', function (req, res, next) {
+  
+    // Delete the user with the persistence engine
+    studentsSave.delete(req.params.id, function (error, student) {
+  
+      // If there are any errors, pass them to next in the correct format
+      if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+  
+      // Send a 200 OK response
+      res.send(200)
+    })
+  })
+  
+//------------------------------------------------------------------------------//
+                      // Delete all student records in the system
+//------------------------------------------------------------------------------//
+
+server.get('/sendDelete', function (req, res, next) {
+  
+        console.log("sendDelete: received request..");
+
+        // Find every entity within the given collection
+        studentsSave.delete({}, function (error, students) {
+ //      Send a 200 OK response
+        res.send(200)
+        })
+      })
